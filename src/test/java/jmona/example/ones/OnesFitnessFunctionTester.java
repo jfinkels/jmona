f6435d324773c39aed619c595d988fc7fb49893a
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-10-09 17:47:14 -0400

    added epsilon variable

--- src/test/java/jmona/example/ones/OnesFitnessFunctionTester.java
+++ src/test/java/jmona/example/ones/OnesFitnessFunctionTester.java
@@ -23,16 +23,18 @@ public class OnesFitnessFunctionTester {
   public void testFitness() {
     final OnesFitnessFunction function = new OnesFitnessFunction();
     OnesIndividual individual = new OnesIndividual(new short[] { 0, 1, 0, 1 });
-    assertEquals(2, function.fitness(individual), 0);
+    
+    final double epsilon = 0;
+    assertEquals(2, function.fitness(individual), epsilon);
 
     individual = new OnesIndividual(new short[] { 1, 0, 1, 0 });
-    assertEquals(2, function.fitness(individual), 0);
+    assertEquals(2, function.fitness(individual), epsilon);
 
     individual = new OnesIndividual(new short[] { 1, 1, 1, 1 });
-    assertEquals(4, function.fitness(individual), 0);
+    assertEquals(4, function.fitness(individual), epsilon);
 
     individual = new OnesIndividual(new short[] {});
-    assertEquals(0, function.fitness(individual), 0);
+    assertEquals(0, function.fitness(individual), epsilon);
   }
 
 }
