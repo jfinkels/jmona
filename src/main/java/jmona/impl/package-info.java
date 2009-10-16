68caedcee357a58e9112387c744ffe1dae5fb85e
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-10-16 17:08:50 -0400

    trying to fix links in javadocs

--- src/main/java/jmona/impl/package-info.java
+++ src/main/java/jmona/impl/package-info.java
@@ -1,8 +1,8 @@
 /**
  * Provides a default implementation of the genetic algorithms interface with
  * some common necessary functionality. The most important class is the
- * {@link DefaultEvolutionContext}, which provides the context in which an
- * evolution can run.
+ * {@link jmona.impl.DefaultEvolutionContext}, which provides the context in
+ * which an evolution can run.
  * 
  * @author jeff
  */
--- src/main/java/jmona/package-info.java
+++ src/main/java/jmona/package-info.java
@@ -1,7 +1,7 @@
 /**
  * Provides an interface for a genetic algorithm. The most important interface
- * is the {@link EvolutionContext} interface, which defines the context in which
- * the evolution will run.
+ * is the {@link jmona.EvolutionContext} interface, which defines the context in
+ * which the evolution will run.
  * 
  * @author jeff
  */
