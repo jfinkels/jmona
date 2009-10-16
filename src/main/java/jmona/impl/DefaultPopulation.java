cc07b5d28bcbd9feaa08aaf98f53c1e3dd161471
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-10-16 17:10:04 -0400

    removed unused constructors

--- src/main/java/jmona/impl/DefaultPopulation.java
+++ src/main/java/jmona/impl/DefaultPopulation.java
@@ -3,7 +3,6 @@
  */
 package jmona.impl;
 
-import java.util.Collection;
 import java.util.Vector;
 
 import jmona.Individual;
@@ -21,24 +20,4 @@ public class DefaultPopulation<T extends Individual> extends Vector<T>
 
   /** Default generated serial version UID. */
   private static final long serialVersionUID = -3999577414252879241L;
-
-  /**
-   * Instantiate this population by calling the default constructor of the
-   * superclass.
-   */
-  public DefaultPopulation() {
-    super();
-  }
-
-  /**
-   * Instantiate this population with the specified initial collection of
-   * individuals.
-   * 
-   * @param initialCollection
-   *          The initial collection of individuals.
-   */
-  public DefaultPopulation(final Collection<T> initialCollection) {
-    super(initialCollection);
-  }
-
 }
