1092800a264abb8ff36e5abf241240924870452d
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-10-18 12:12:27 -0400

    changed test to test many individuals and determine arithmetic mean number of mutations per individual

--- src/test/java/jmona/example/ones/OnesMutatorFunctionTester.java
+++ src/test/java/jmona/example/ones/OnesMutatorFunctionTester.java
@@ -5,6 +5,10 @@ package jmona.example.ones;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+
+import java.util.List;
+import java.util.Vector;
+
 import jmona.MutationException;
 
 import org.junit.Test;
@@ -18,7 +22,9 @@ public class OnesMutatorFunctionTester {
 
   /** The length of the genes of the individuals to test. */
   public static final int GENE_LENGTH = 100;
-  
+  /** The number of Individuals on which to test mutation. */
+  public static final int NUM_INDIVIDUALS = 100;
+
   /**
    * Test method for
    * {@link jmona.example.ones.OnesMutatorFunction#mutate(jmona.example.ones.OnesIndividual)}
@@ -29,36 +35,58 @@ public class OnesMutatorFunctionTester {
     // create the mutator function
     final OnesMutatorFunction function = new OnesMutatorFunction();
 
-    // initialize an array as a gene for an individual
-    final short[] array = new short[GENE_LENGTH];
-    for (int i = 0; i < GENE_LENGTH; ++i) {
-      array[i] = 0;
+    // create a list of all individuals.
+    final List<OnesIndividual> allIndividuals = new Vector<OnesIndividual>();
+
+    // initialize many genes for many individuals
+    for (int i = 0; i < NUM_INDIVIDUALS; ++i) {
+
+      // initialize an array as a gene for an individual
+      final short[] array = new short[GENE_LENGTH];
+      for (int j = 0; j < GENE_LENGTH; ++j) {
+        array[j] = 0;
+      }
+
+      // create an individual with the created gene
+      allIndividuals.add(new OnesIndividual(array));
     }
 
-    // create an individual with a gene
-    final OnesIndividual individual = new OnesIndividual(array);
-
-    // mutate the individual's gene
-    try {
-      function.mutate(individual);
-    } catch (final MutationException exception) {
-      exception.printStackTrace(System.err);
-      fail(exception.getMessage());
-    }
-
-    final double prob = OnesMutatorFunction.PROB_BITWISE_MUTATION;
-    final double expectedMutations = prob * individual.gene().length;
-    final double epsilon = expectedMutations * .75;
-
-    double actualMutations = 0;
-    for (int i = 0; i < GENE_LENGTH; ++i) {
-      if (individual.gene()[i] == 1) {
-        actualMutations += 1;
+    for (final OnesIndividual individual : allIndividuals) {
+      // mutate the individual's gene
+      try {
+        function.mutate(individual);
+      } catch (final MutationException exception) {
+        exception.printStackTrace(System.err);
+        fail(exception.getMessage());
       }
     }
 
-    // TODO use standard deviation or something more official for epsilon
-    assertEquals(expectedMutations, actualMutations, epsilon);
-  }
+    // iterate over all individuals
+    double result = 0;
+    for (final OnesIndividual individual : allIndividuals) {
 
+      // iterate over each bit in the gene of the individual
+      double actualMutations = 0;
+      for (int i = 0; i < GENE_LENGTH; ++i) {
+        // increment the number of mutations each time there is a one
+        actualMutations += individual.gene()[i];
+      }
+
+      // get the total number of mutations over all individuals
+      result += actualMutations;
+    }
+
+    // determine the arithmetic mean of mutations over all individuals
+    result /= (double) allIndividuals.size();
+
+    // determine the expected average mutations
+    final double expectedMutations = OnesMutatorFunction.PROB_BITWISE_MUTATION
+        * GENE_LENGTH;
+
+    // the error tolerance
+    final double epsilon = expectedMutations * .10;
+
+    // TODO use standard deviation or something more official for epsilon
+    assertEquals(expectedMutations, result, epsilon);
+  }
 }
