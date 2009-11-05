a87ac4cd26dea013896fb6dad345752cd9ea9ac9
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-11-04 20:13:27 -0500

    added selectRandomFromSet method

--- src/main/java/jmona/impl/Util.java
+++ src/main/java/jmona/impl/Util.java
@@ -19,16 +19,44 @@
  */
 package jmona.impl;
 
+import java.util.Iterator;
 import java.util.Random;
+import java.util.Set;
 
 /**
- * Utility class containing a random number generator.
+ * Utility class containing objects and methods necessary for randomness.
  * 
  * @author jfinke
  */
 public class Util {
   /** Random number generator. */
   public static final Random RANDOM = new Random();
+
+  /**
+   * Return an element from the set chosen with uniform distribution over all
+   * elements.
+   * 
+   * @param <T>
+   *          The type of element in the Set.
+   * @param set
+   *          The set from which to randomly choose an element.
+   * @return An element chosen with uniform probability over all elements, or
+   *         {@code null} if something crazy happened.
+   */
+  public static final <T> T randomFromSet(final Set<T> set) {
+    int selection = RANDOM.nextInt(set.size());
+    final Iterator<T> iterator = set.iterator();
+    T element = null;
+    while (iterator.hasNext()) {
+      element = iterator.next();
+      if (selection == 0) {
+        return element;
+      } else {
+        selection -= 1;
+      }
+    }
+    return null;
+  }
 
   /** Instantiation disallowed. */
   protected Util() {
