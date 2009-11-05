3046bc9d3bb36bfb71c2785bd53959fb870d3fa2
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-11-05 12:01:36 -0500

    added unimplemented tests

---
+++ src/test/java/jmona/gp/impl/GPEvolutionContextTester.java
@@ -0,0 +1,47 @@
+/**
+ * GPEvolutionContextTester.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.gp.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jfinkels
+ */
+public class GPEvolutionContextTester {
+
+  /**
+   * Test method for {@link jmona.gp.impl.GPEvolutionContext#GPEvolutionContext(jmona.Population)}.
+   */
+  @Test
+  public void testGPEvolutionContext() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.GPEvolutionContext#stepGeneration()}.
+   */
+  @Test
+  public void testStepGeneration() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ src/test/java/jmona/gp/impl/GPMutationFunctionTester.java
@@ -0,0 +1,47 @@
+/**
+ * GPMutationFunctionTester.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.gp.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jfinkels
+ */
+public class GPMutationFunctionTester {
+
+  /**
+   * Test method for {@link jmona.gp.impl.GPMutationFunction#setTreeFactory(jmona.gp.TreeFactory)}.
+   */
+  @Test
+  public void testSetTreeFactory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.GPMutationFunction#mutate(jmona.gp.Tree)}.
+   */
+  @Test
+  public void testMutate() {
+    fail("Not yet implemented");
+  }
+
+}
