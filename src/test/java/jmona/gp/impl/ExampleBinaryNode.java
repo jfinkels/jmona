44aea2779c5de176c23791e5bc79e7ddafd44eb9
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-11-09 17:55:27 -0500

    added another Example class

---
+++ src/test/java/jmona/gp/impl/ExampleBinaryNode.java
@@ -0,0 +1,36 @@
+/**
+ * ExampleBinaryNode.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.gp.impl;
+
+/**
+ * An example BinaryNode for use in testing which utilizes the
+ * ExampleBinaryOperation.
+ * 
+ * @author jfinkels
+ */
+public class ExampleBinaryNode extends BinaryNode<Integer> {
+  /**
+   * Instantiate this Node by providing an ExampleBinaryOperation object to the
+   * constructor of the superclass.
+   */
+  public ExampleBinaryNode() {
+    super(new ExampleBinaryOperation());
+  }
+}
