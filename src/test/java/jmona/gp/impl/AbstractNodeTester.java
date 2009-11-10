c6ecf0738d49f7ff9425385daa8e6c1bb8f24764
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-11-10 01:14:46 -0500

    added more skeleton test classes

---
+++ src/test/java/jmona/gp/impl/AbstractNodeTester.java
@@ -0,0 +1,71 @@
+/**
+ * AbstractNodeTester.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.gp.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jfinkels
+ */
+public class AbstractNodeTester {
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractNode#parent()}.
+   */
+  @Test
+  public void testParent() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractNode#replaceThisNodeWith(jmona.gp.Node)}.
+   */
+  @Test
+  public void testReplaceThisNodeWithNodeOfV() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractNode#replaceThisNodeWith(jmona.gp.Tree)}.
+   */
+  @Test
+  public void testReplaceThisNodeWithTreeOfV() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractNode#setParent(jmona.gp.Node)}.
+   */
+  @Test
+  public void testSetParent() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractNode#swapWith(jmona.gp.Node)}.
+   */
+  @Test
+  public void testSwapWith() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ src/test/java/jmona/gp/impl/AbstractTreeFactoryTester.java
@@ -0,0 +1,111 @@
+/**
+ * AbstractTreeFactoryTester.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.gp.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jfinkels
+ */
+public class AbstractTreeFactoryTester {
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#createIndividual()}.
+   */
+  @Test
+  public void testCreateIndividual() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#createTree(int)}.
+   */
+  @Test
+  public void testCreateTree() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#functionNodeFactory()}.
+   */
+  @Test
+  public void testFunctionNodeFactory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#maxDepth()}.
+   */
+  @Test
+  public void testMaxDepth() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#setFunctionNodeFactory(jmona.gp.FunctionNodeFactory)}.
+   */
+  @Test
+  public void testSetFunctionNodeFactory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#setMaxDepth(int)}.
+   */
+  @Test
+  public void testSetMaxDepth() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#setTerminalNodeFactory(jmona.gp.TerminalNodeFactory)}.
+   */
+  @Test
+  public void testSetTerminalNodeFactory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#setTreeClass(java.lang.Class)}.
+   */
+  @Test
+  public void testSetTreeClass() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#terminalNodeFactory()}.
+   */
+  @Test
+  public void testTerminalNodeFactory() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.gp.impl.AbstractTreeFactory#treeClass()}.
+   */
+  @Test
+  public void testTreeClass() {
+    fail("Not yet implemented");
+  }
+
+}
