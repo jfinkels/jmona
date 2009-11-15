ea0a6655570dfb970a3495a55b352d139490ee5e
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-11-15 11:49:18 -0500

    added unimplemented test class for AbstractEvolutionContext class

---
+++ src/test/java/jmona/impl/AbstractEvolutionContextTester.java
@@ -0,0 +1,191 @@
+/**
+ * AbstractEvolutionContextTester.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author jfinkels
+ */
+public class AbstractEvolutionContextTester {
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#AbstractEvolutionContext(jmona.Population)}.
+   */
+  @Test
+  public void testAbstractEvolutionContext() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#sanityCheck()}.
+   */
+  @Test
+  public void testSanityCheck() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#crossoverFunction()}.
+   */
+  @Test
+  public void testCrossoverFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#crossoverProbability()}.
+   */
+  @Test
+  public void testCrossoverProbability() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#currentFitnesses()}.
+   */
+  @Test
+  public void testCurrentFitnesses() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#currentGeneration()}.
+   */
+  @Test
+  public void testCurrentGeneration() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#currentPopulation()}.
+   */
+  @Test
+  public void testCurrentPopulation() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#fitnessFunction()}.
+   */
+  @Test
+  public void testFitnessFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#incrementGeneration()}.
+   */
+  @Test
+  public void testIncrementGeneration() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#mutationFunction()}.
+   */
+  @Test
+  public void testMutationFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#recalculateFitnesses()}.
+   */
+  @Test
+  public void testRecalculateFitnesses() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#selectionFunction()}.
+   */
+  @Test
+  public void testSelectionFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setCrossoverFunction(jmona.CrossoverFunction)}.
+   */
+  @Test
+  public void testSetCrossoverFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setCrossoverProbability(double)}.
+   */
+  @Test
+  public void testSetCrossoverProbability() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setCurrentPopulation(jmona.Population)}.
+   */
+  @Test
+  public void testSetCurrentPopulation() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setFitnessFunction(jmona.FitnessFunction)}.
+   */
+  @Test
+  public void testSetFitnessFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setMutationProbability(double)}.
+   */
+  @Test
+  public void testSetMutationProbability() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#mutationProbability()}.
+   */
+  @Test
+  public void testMutationProbability() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setMutationFunction(jmona.MutationFunction)}.
+   */
+  @Test
+  public void testSetMutationFunction() {
+    fail("Not yet implemented");
+  }
+
+  /**
+   * Test method for {@link jmona.impl.AbstractEvolutionContext#setSelectionFunction(jmona.SelectionFunction)}.
+   */
+  @Test
+  public void testSetSelectionFunction() {
+    fail("Not yet implemented");
+  }
+
+}
