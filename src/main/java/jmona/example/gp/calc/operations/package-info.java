bfc968573fff0da7b4875ab1c41329cc09566ba5
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-05 16:09:26 -0500

    added package level documentation files

---
+++ src/main/java/jmona/example/gp/calc/operations/package-info.java
@@ -0,0 +1,28 @@
+/**
+ * package-info.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * Provides classes which encapsulate operations on functions (addition,
+ * subtraction, multiplication, and division) which are used when evaluating a
+ * {@link jmona.gp.Tree} in the Calc evolution.
+ * 
+ * @author jfinkels
+ */
+package jmona.example.gp.calc.operations;
+
