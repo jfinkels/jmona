f2cf31f09123f8a89d946c5189e017067d27827c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-06 21:01:03 -0500

    added tests

--- src/test/java/jmona/game/impl/DefaultTwoPlayerGameResultTester.java
+++ src/test/java/jmona/game/impl/DefaultTwoPlayerGameResultTester.java
@@ -19,61 +19,103 @@
  */
 package jmona.game.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertSame;
+import jmona.game.impl.example.ExampleStrategy;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Test class for the DefaultTwoPlayerGameResult class.
+ * 
  * @author jfinkels
  */
 public class DefaultTwoPlayerGameResultTester {
 
+  /** The score for strategy 1. */
+  public static final double SCORE1 = 2.0;
+  /** The score for strategy 2. */
+  public static final double SCORE2 = 1.0;
+  /** An example strategy. */
+  public static final ExampleStrategy STRATEGY1 = new ExampleStrategy();
+  /** An example strategy. */
+  public static final ExampleStrategy STRATEGY2 = new ExampleStrategy();
+  /** Zero. */
+  public static final double ZERO_DELTA = 0.0;
+  /** The GameResult under test. */
+  private DefaultTwoPlayerGameResult<ExampleStrategy> gameResult = null;
+
+  /** Establish a fixture for tests in this class. */
+  @Before
+  public final void setUp() {
+    this.gameResult = new DefaultTwoPlayerGameResult<ExampleStrategy>();
+    this.gameResult.setScoreOfStrategy1(SCORE1);
+    this.gameResult.setScoreOfStrategy2(SCORE2);
+    this.gameResult.setWinner(STRATEGY2);
+  }
+
   /**
-   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#scoreOfStrategy1()}.
+   * Test method for
+   * {@link jmona.game.impl.DefaultTwoPlayerGameResult#scoreOfStrategy1()}.
    */
   @Test
   public void testScoreOfStrategy1() {
-    fail("Not yet implemented");
+    assertEquals(SCORE1, this.gameResult.scoreOfStrategy1(), ZERO_DELTA);
   }
 
   /**
-   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#scoreOfStrategy2()}.
+   * Test method for
+   * {@link jmona.game.impl.DefaultTwoPlayerGameResult#scoreOfStrategy2()}.
    */
   @Test
   public void testScoreOfStrategy2() {
-    fail("Not yet implemented");
+    assertEquals(SCORE2, this.gameResult.scoreOfStrategy2(), ZERO_DELTA);
   }
 
   /**
-   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#setScoreOfStrategy1(double)}.
+   * Test method for
+   * {@link jmona.game.impl.DefaultTwoPlayerGameResult#setScoreOfStrategy1(double)}
+   * .
    */
   @Test
   public void testSetScoreOfStrategy1() {
-    fail("Not yet implemented");
+    final double newScore = 10.0;
+    this.gameResult.setScoreOfStrategy1(newScore);
+    assertEquals(newScore, this.gameResult.scoreOfStrategy1(), ZERO_DELTA);
   }
 
   /**
-   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#setScoreOfStrategy2(double)}.
+   * Test method for
+   * {@link jmona.game.impl.DefaultTwoPlayerGameResult#setScoreOfStrategy2(double)}
+   * .
    */
   @Test
   public void testSetScoreOfStrategy2() {
-    fail("Not yet implemented");
+    final double newScore = 14.0;
+    this.gameResult.setScoreOfStrategy2(newScore);
+    assertEquals(newScore, this.gameResult.scoreOfStrategy2(), ZERO_DELTA);
   }
 
   /**
-   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#setWinner(jmona.game.Strategy)}.
+   * Test method for
+   * {@link jmona.game.impl.DefaultTwoPlayerGameResult#setWinner(jmona.game.Strategy)}
+   * .
    */
   @Test
   public void testSetWinner() {
-    fail("Not yet implemented");
+    final ExampleStrategy newWinner = new ExampleStrategy();
+    this.gameResult.setWinner(newWinner);
+    assertSame(newWinner, this.gameResult.winner());
   }
 
   /**
-   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#winner()}.
+   * Test method for {@link jmona.game.impl.DefaultTwoPlayerGameResult#winner()}
+   * .
    */
   @Test
   public void testWinner() {
-    fail("Not yet implemented");
+    assertSame(STRATEGY2, this.gameResult.winner());
   }
 
 }
