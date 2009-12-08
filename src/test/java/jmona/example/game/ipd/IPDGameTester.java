7cd88cb71acceae69b3b198dd256b804fd29ab0d
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-08 13:15:28 -0500

    started writing tests

--- src/test/java/jmona/example/game/ipd/IPDGameTester.java
+++ src/test/java/jmona/example/game/ipd/IPDGameTester.java
@@ -19,14 +19,71 @@
  */
 package jmona.example.game.ipd;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import jmona.example.game.ipd.strategy.CooperativeStrategy;
+import jmona.example.game.ipd.strategy.IPDStrategy;
+import jmona.example.game.ipd.strategy.RuthlessStrategy;
+import jmona.game.GameplayException;
+import jmona.game.TwoPlayerGameResult;
+import jmona.test.Util;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 /**
+ * Test class for the IPDGame class.
+ * 
  * @author jfinkels
  */
 public class IPDGameTester {
+
+  /** The Logger for this class. */
+  private static final transient Logger LOG = Logger
+      .getLogger(IPDGameTester.class);
+
+  /** The game under test. */
+  private IPDGame game = null;
+
+  /** Establish a fixture for tests in this class. */
+  public final void setUp() {
+    this.game = new IPDGame();
+  }
+
+  /**
+   * Test method for
+   * {@link jmona.example.game.ipd.IPDGame#play(jmona.example.game.ipd.strategy.IPDStrategy, jmona.example.game.ipd.strategy.IPDStrategy)}
+   * .
+   */
+  @Test
+  public void testPlay() {
+    fail("Not yet implemented");
+    IPDStrategy strategy1 = new CooperativeStrategy();
+    IPDStrategy strategy2 = new CooperativeStrategy();
+    TwoPlayerGameResult<IPDStrategy> result = null;
+    try {
+      result = this.game.play(strategy1, strategy2);
+    } catch (final GameplayException exception) {
+      Util.fail(exception);
+    }
+
+    LOG.debug(result);
+
+    strategy2 = new RuthlessStrategy();
+    try {
+      result = this.game.play(strategy1, strategy2);
+    } catch (final GameplayException exception) {
+      Util.fail(exception);
+    }
+    
+    strategy1 = new RuthlessStrategy();
+    try {
+      result = this.game.play(strategy1, strategy2);
+    } catch (final GameplayException exception) {
+      Util.fail(exception);
+    }
+
+    LOG.debug(result);
+  }
 
   /**
    * Test method for {@link jmona.example.game.ipd.IPDGame#setIterations(int)}.
@@ -34,46 +91,73 @@ public class IPDGameTester {
   @Test
   public void testSetIterations() {
     fail("Not yet implemented");
+    int newIterations = 0;
+    this.game.setIterations(newIterations);
+
+    newIterations = 1;
+    this.game.setIterations(newIterations);
+
+    newIterations = 2;
+    this.game.setIterations(newIterations);
   }
 
   /**
-   * Test method for {@link jmona.example.game.ipd.IPDGame#setRewardForMutualCooperation(int)}.
-   */
-  @Test
-  public void testSetRewardForMutualCooperation() {
-    fail("Not yet implemented");
-  }
-
-  /**
-   * Test method for {@link jmona.example.game.ipd.IPDGame#setPunishmentForMutualDefection(int)}.
+   * Test method for
+   * {@link jmona.example.game.ipd.IPDGame#setPunishmentForMutualDefection(int)}
+   * .
    */
   @Test
   public void testSetPunishmentForMutualDefection() {
     fail("Not yet implemented");
+    int punishmentPayoff = 0;
+    this.game.setPunishmentForMutualDefection(punishmentPayoff);
+
+    punishmentPayoff = 10;
+    this.game.setPunishmentForMutualDefection(punishmentPayoff);
   }
 
   /**
-   * Test method for {@link jmona.example.game.ipd.IPDGame#setSuckersPayoff(int)}.
+   * Test method for
+   * {@link jmona.example.game.ipd.IPDGame#setRewardForMutualCooperation(int)}.
+   */
+  @Test
+  public void testSetRewardForMutualCooperation() {
+    fail("Not yet implemented");
+    int rewardPayoff = 0;
+    this.game.setRewardForMutualCooperation(rewardPayoff);
+
+    rewardPayoff = 10;
+    this.game.setRewardForMutualCooperation(rewardPayoff);
+  }
+
+  /**
+   * Test method for
+   * {@link jmona.example.game.ipd.IPDGame#setSuckersPayoff(int)}.
    */
   @Test
   public void testSetSuckersPayoff() {
     fail("Not yet implemented");
+
+    int newSuckersPayoff = 0;
+    this.game.setSuckersPayoff(newSuckersPayoff);
+
+    newSuckersPayoff = 10;
+    this.game.setSuckersPayoff(newSuckersPayoff);
   }
 
   /**
-   * Test method for {@link jmona.example.game.ipd.IPDGame#setTemptationToDefect(int)}.
+   * Test method for
+   * {@link jmona.example.game.ipd.IPDGame#setTemptationToDefect(int)}.
    */
   @Test
   public void testSetTemptationToDefect() {
     fail("Not yet implemented");
-  }
 
-  /**
-   * Test method for {@link jmona.example.game.ipd.IPDGame#play(jmona.example.game.ipd.strategy.IPDStrategy, jmona.example.game.ipd.strategy.IPDStrategy)}.
-   */
-  @Test
-  public void testPlay() {
-    fail("Not yet implemented");
+    int newTemptationPayoff = 0;
+    this.game.setTemptationToDefect(newTemptationPayoff);
+
+    newTemptationPayoff = 10;
+    this.game.setTemptationToDefect(newTemptationPayoff);
   }
 
 }
