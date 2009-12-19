843595ca371e8ec6e81211ca2f948d94e1916f60
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-19 13:53:30 -0500

    first implementation of tournament selection

--- src/main/java/jmona/impl/selection/TournamentSelection.java
+++ src/main/java/jmona/impl/selection/TournamentSelection.java
@@ -19,10 +19,14 @@
  */
 package jmona.impl.selection;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.Vector;
 
 import jmona.Individual;
 import jmona.SelectionFunction;
+import jmona.impl.Util;
 
 /**
  * A tournament selection algorithm.
@@ -35,6 +39,48 @@ public class TournamentSelection<T extends Individual> implements
     SelectionFunction<T> {
 
   /**
+   * The default number of Individuals to be chosen at random to compete in the
+   * tournament.
+   */
+  public static final int DEFAULT_TOURNAMENT_SIZE = 10;
+  /**
+   * The number of Individuals to be chosen at random to compete in the
+   * tournament.
+   */
+  private int tournamentSize = DEFAULT_TOURNAMENT_SIZE;
+
+  /**
+   * The default probability of selecting the top competitor in the tournament.
+   */
+  public static final double DEFAULT_SELECTION_PROBABILITY = 0.9;
+  /**
+   * The probability of selecting the top competitor in the tournament.
+   * 
+   * Let {@code p} be the value of this field. Then the probability of selecting
+   * the second place competitor in the tournament is {@code p*(1-p)}, the
+   * probability of selecting the third place competitor is {@code p*(p*(1-p))},
+   * and so on.
+   */
+  // TODO is that math correct?
+  private double selectionProbability = DEFAULT_SELECTION_PROBABILITY;
+
+  /**
+   * Set the number of Individuals to be chosen at random to compete in the
+   * tournament.
+   * 
+   * @param newTournamentSize
+   *          The number of Individuals to be chosen at random to compete in the
+   *          tournament.
+   */
+  public void setTournamentSize(final int newTournamentSize) {
+    this.tournamentSize = newTournamentSize;
+  }
+
+  /**
+   * 
+   * Currently chooses competitors for the tournament randomly
+   * <em>with replacement</em>, so the one Individual could be in mutiple
+   * competitor spots in the tournament.
    * 
    * @param fitnesses
    *          {@inheritDoc}
@@ -43,8 +89,44 @@ public class TournamentSelection<T extends Individual> implements
   // TODO documentation for this method
   @Override
   public T select(final Map<T, Double> fitnesses) {
-    // TODO Auto-generated method stub
-    return null;
+    // initialize a list of competitors for the tournament
+    final List<T> competitors = new Vector<T>();
+
+    // fill the list of competitors with random Individuals (with replacement)
+    // TODO allow random without replacement
+    while (competitors.size() < this.tournamentSize) {
+      competitors.add(Util.randomFromSet(fitnesses.keySet()));
+    }
+
+    // sort the list of competitors based on their fitnesses
+    Collections.sort(competitors, new FitnessComparator<T>(fitnesses));
+
+    // choose a random double
+    final double choice = Util.RANDOM.nextDouble();
+
+    // the initial probability of choosing the top competitor
+    double probability = this.selectionProbability;
+
+    // the competitor which will eventually be selected
+    int chosenCompetitor = 0;
+
+    // until the chosen random number is less than 'probability'...
+    while (choice > probability || chosenCompetitor >= competitors.size()) {
+      // increment the probability
+      // TODO double check this math!!! check out how JGAP does it
+      probability += probability * (1 - this.selectionProbability);
+
+      // increment the number of the competitor to select as winner
+      chosenCompetitor += 1;
+    }
+
+    // if the chosen competitor is greater than the number of competitors, just
+    // choose the last competitor
+    if (chosenCompetitor >= competitors.size()) {
+      chosenCompetitor = competitors.size() - 1;
+    }
+
+    return competitors.get(chosenCompetitor);
   }
 
 }
