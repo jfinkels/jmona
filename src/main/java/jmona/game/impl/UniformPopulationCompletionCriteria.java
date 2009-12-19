81441651e7a6dd90521b81b07bf632be72cfe53c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-18 22:32:46 -0500

    now extends DefaultCompletionCriteria

--- src/main/java/jmona/game/impl/UniformPopulationCompletionCriteria.java
+++ src/main/java/jmona/game/impl/UniformPopulationCompletionCriteria.java
@@ -19,26 +19,43 @@
  */
 package jmona.game.impl;
 
-import jmona.CompletionCriteria;
 import jmona.CompletionException;
 import jmona.EvolutionContext;
 import jmona.Population;
 import jmona.game.Strategy;
+import jmona.impl.DefaultCompletionCriteria;
 
 /**
+ * Determines whether a Population in an EvolutionContext contains only
+ * Individuals of one class.
+ * 
+ * @param <S>
+ *          The type of Strategy in the evolution to be checked for completion.
  * @author jfinkels
  */
-public class UniformPopulationCompletionCriteria<S extends Strategy> implements
-    CompletionCriteria<S> {
+public class UniformPopulationCompletionCriteria<S extends Strategy> extends
+    DefaultCompletionCriteria<S> {
 
-  /*
-   * (non-Javadoc)
+  /**
+   * Determines whether the current Population in the specified EvolutionContext
+   * contains only Individuals of one class.
    * 
+   * @param context
+   *          The EvolutionContext containing the Population.
+   * @return Whether the current Population in the specified EvolutionContext
+   *         contains only Individuals of one class.
+   * @throws CompletionException
+   *           If this method in the superclass throws a CompletionException.
    * @see jmona.CompletionCriteria#isSatisfied(jmona.EvolutionContext)
    */
   @Override
   public boolean isSatisfied(final EvolutionContext<S> context)
       throws CompletionException {
+
+    // if the default criteria are met, then return true
+    if (super.isSatisfied(context)) {
+      return true;
+    }
 
     // get the current population from the evolution context
     final Population<S> population = context.currentPopulation();
