ecb9761d943ac352c1b7dd187f6303cbfd74c81c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-18 23:58:36 -0500

    added PolynomialEvolutionTester

--- src/test/java/jmona/example/gp/calc/AllTest.java
+++ src/test/java/jmona/example/gp/calc/AllTest.java
@@ -30,7 +30,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 @RunWith(Suite.class)
 @SuiteClasses({ CalcEvolutionTester.class,
-    FunctionEquivalenceTesterTester.class })
+    FunctionEquivalenceTesterTester.class, PolynomialEvolutionTester.class })
 public class AllTest {
 
 }
