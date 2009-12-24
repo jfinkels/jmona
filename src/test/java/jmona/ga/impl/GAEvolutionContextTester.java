d25556820b0af2724ccc484712968b6b7e23cfab
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-24 16:04:58 -0500

    added tests

--- src/test/java/jmona/ga/impl/GAEvolutionContextTester.java
+++ src/test/java/jmona/ga/impl/GAEvolutionContextTester.java
@@ -22,10 +22,17 @@ package jmona.ga.impl;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import jmona.CrossoverException;
 import jmona.EvolutionException;
 import jmona.FitnessException;
+import jmona.MutationException;
 import jmona.Population;
+import jmona.SelectionException;
 import jmona.impl.DefaultPopulation;
+import jmona.impl.example.ExampleBadCrossoverFunction;
+import jmona.impl.example.ExampleBadFitnessFunction;
+import jmona.impl.example.ExampleBadMutationFunction;
+import jmona.impl.example.ExampleBadSelectionFunction;
 import jmona.impl.example.ExampleCrossoverFunction;
 import jmona.impl.example.ExampleFitnessFunction;
 import jmona.impl.example.ExampleIndividual;
@@ -41,6 +48,76 @@ import org.junit.Test;
  * @author jfinkels
  */
 public class GAEvolutionContextTester {
+
+  /** Test for throwing exceptions during the evolution. */
+  @Test
+  public void testExceptions() {
+    // create three individuals to put into a population
+    final ExampleIndividual individual1 = new ExampleIndividual();
+    final ExampleIndividual individual2 = new ExampleIndividual();
+    final ExampleIndividual individual3 = new ExampleIndividual();
+
+    // put those three individuals in a population
+    final Population<ExampleIndividual> population = new DefaultPopulation<ExampleIndividual>();
+    population.add(individual1);
+    population.add(individual2);
+    population.add(individual3);
+
+    // instantiate a new EvolutionContext
+    final GAEvolutionContext<ExampleIndividual> context = new GAEvolutionContext<ExampleIndividual>(
+        population);
+
+    // set the necessary functions on the context
+    context.setCrossoverFunction(new ExampleBadCrossoverFunction());
+    context.setMutationFunction(new ExampleBadMutationFunction());
+    context.setSelectionFunction(new ExampleBadSelectionFunction());
+
+    try {
+      context.setFitnessFunction(new ExampleBadFitnessFunction());
+      Util.shouldHaveThrownException();
+    } catch (final FitnessException exception) {
+      assertTrue(exception instanceof FitnessException);
+      try {
+        context.setFitnessFunction(new ExampleFitnessFunction());
+      } catch (final FitnessException exception2) {
+        Util.fail(exception2);
+      }
+    }
+
+    context.setMutationProbability(1);
+    context.setCrossoverProbability(1);
+
+    try {
+      context.stepGeneration();
+      Util.shouldHaveThrownException();
+    } catch (final EvolutionException exception) {
+      assertTrue(exception.getCause() instanceof SelectionException);
+      context
+          .setSelectionFunction(new FitnessProportionateSelection<ExampleIndividual>());
+    }
+
+    try {
+      context.stepGeneration();
+      Util.shouldHaveThrownException();
+    } catch (final EvolutionException exception) {
+      assertTrue(exception.getCause() instanceof CrossoverException);
+      context.setCrossoverFunction(new ExampleCrossoverFunction());
+    }
+
+    try {
+      context.stepGeneration();
+      Util.shouldHaveThrownException();
+    } catch (final EvolutionException exception) {
+      assertTrue(exception.getCause() instanceof MutationException);
+      context.setMutationFunction(new ExampleMutationFunction());
+    }
+
+    try {
+      context.stepGeneration();
+    } catch (final EvolutionException exception) {
+      Util.fail(exception);
+    }
+  }
 
   /**
    * Test method for
@@ -86,13 +163,13 @@ public class GAEvolutionContextTester {
     } catch (final EvolutionException exception) {
       Util.fail(exception);
     }
-    
+
     assertEquals(population.size(), context.currentPopulation().size());
 
     assertFalse(context.currentPopulation().contains(individual1));
     assertFalse(context.currentPopulation().contains(individual2));
     assertFalse(context.currentPopulation().contains(individual3));
-    
+
     context.currentPopulation().remove(0);
     try {
       context.executeGenerationStep();
@@ -101,7 +178,7 @@ public class GAEvolutionContextTester {
     }
 
     assertEquals(population.size() - 1, context.currentPopulation().size());
-    
+
     context.currentPopulation().remove(0);
     try {
       context.executeGenerationStep();
@@ -121,5 +198,4 @@ public class GAEvolutionContextTester {
     }
 
   }
-
 }
