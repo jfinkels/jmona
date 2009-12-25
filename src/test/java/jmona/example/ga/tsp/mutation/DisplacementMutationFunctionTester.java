9274690059663b2374d3be55d6fd503732b99a04
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-24 23:18:39 -0500

    fixed assertions

--- src/test/java/jmona/example/ga/tsp/mutation/DisplacementMutationFunctionTester.java
+++ src/test/java/jmona/example/ga/tsp/mutation/DisplacementMutationFunctionTester.java
@@ -20,12 +20,15 @@
 package jmona.example.ga.tsp.mutation;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import jmona.MutationException;
 import jmona.test.Util;
 
 import org.junit.Test;
 
 /**
+ * Test class for the DisplacementMutationFunction class.
+ * 
  * @author jfinkels
  */
 public class DisplacementMutationFunctionTester extends
@@ -54,9 +57,17 @@ public class DisplacementMutationFunctionTester extends
         Util.fail(exception);
       }
 
+      // for example:
       // before: 0 1 2 3 4 5 6 7 8 9
-      // after : 0 4 5 6 7 1 2 3 8 9
+      // after : 0|4 5 6 7|1 2 3|8 9
 
+      assertEquals(LENGTH, this.tour().size());
+
+      for (int i = 0; i < LENGTH; ++i) {
+        assertTrue(this.tour().contains(i));
+      }
+
+      // get the index of the start of the first slice
       int start = 0;
       for (int i = 0; i < this.tour().size(); ++i) {
         if (this.tour().get(i) != i) {
@@ -65,22 +76,50 @@ public class DisplacementMutationFunctionTester extends
         }
       }
 
-      /*
-       * int end = 0; for (int i = start + 1; i < this.tour().size(); ++i) { if
-       * (this.tour().get(i) != (this.tour().get(i - 1) + 1)) { end = i; break;
-       * } }
-       */
-
-      // undo the displacement
-      int originalLocation = 0;
-      while (this.tour().get(start) != start) {
-        originalLocation = this.tour().remove(start);
-        this.tour().add(originalLocation, originalLocation);
+      // get the index between the first and second slices
+      int middle = 0;
+      for (int i = start + 1; i < this.tour().size(); ++i) {
+        if (this.tour().get(i) != this.tour().get(i - 1) + 1) {
+          middle = i;
+          break;
+        }
       }
 
-      for (int i = 0; i < this.tour().size(); ++i) {
+      // get the index of the end of the second slice
+      int end = 0;
+      for (int i = middle + 1; i < this.tour().size(); ++i) {
+        if (this.tour().get(i) != this.tour().get(i - 1) + 1) {
+          end = i;
+          break;
+        }
+      }
+
+      // get the length of the swapped slices
+      final int firstLength = middle - start;
+      final int secondLength = end - middle;
+
+      // from the start to the first change
+      for (int i = 0; i < start; ++i) {
         assertEquals(i, this.tour().get(i).intValue());
       }
+
+      // from the first change to the second change
+      for (int i = 0; i < firstLength; ++i) {
+        assertEquals(this.tour().get(start) + i, this.tour().get(start + i)
+            .intValue());
+      }
+
+      // from the second change to the end of the slices
+      for (int i = 0; i < secondLength; ++i) {
+        assertEquals(this.tour().get(middle) + i, this.tour().get(middle + i)
+            .intValue());
+      }
+
+      // from the end of the slices to the end of the tour
+      for (int i = end; i < this.tour().size(); ++i) {
+        assertEquals(i, this.tour().get(i).intValue());
+      }
+
     }
   }
 }
