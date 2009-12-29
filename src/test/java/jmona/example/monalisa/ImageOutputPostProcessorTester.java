9041a23903962c5470628252b810d97df08427e7
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-28 21:57:44 -0500

    removed unused import

--- src/test/java/jmona/example/monalisa/ImageOutputPostProcessorTester.java
+++ src/test/java/jmona/example/monalisa/ImageOutputPostProcessorTester.java
@@ -21,7 +21,6 @@ package jmona.example.monalisa;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.io.File;
 import java.util.List;
