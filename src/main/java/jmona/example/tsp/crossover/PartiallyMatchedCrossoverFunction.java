c96903f7759446caaa8a4b0149e761d89bf92f9a
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-30 01:20:36 -0500

    implemented PMX

--- src/main/java/jmona/example/tsp/crossover/PartiallyMatchedCrossoverFunction.java
+++ src/main/java/jmona/example/tsp/crossover/PartiallyMatchedCrossoverFunction.java
@@ -20,48 +20,123 @@
 package jmona.example.tsp.crossover;
 
 import java.util.List;
-import java.util.Vector;
 
 import jmona.CrossoverFunction;
 import jmona.impl.Util;
 
-import org.apache.log4j.Logger;
-
 /**
+ * Partially matched crossover (also known as PMX), which swaps a slice of the
+ * tours, then uses the permutation defined by the integers in the swapped
+ * slices to replace repeated cities in the non-swapped sections of the tours.
+ * 
  * @author jfinkels
  */
+// TODO inherit from two-point crossover?
 public class PartiallyMatchedCrossoverFunction implements
     CrossoverFunction<List<Integer>> {
 
-  /** The Logger for this class. */
-  private static final transient Logger LOG = Logger
-      .getLogger(PartiallyMatchedCrossoverFunction.class);
-
-  /*
-   * (non-Javadoc)
+  /**
+   * Perform partially matched crossover (also known as PMX) on the specified
+   * tours.
    * 
-   * @see jmona.CrossoverFunction#crossover(jmona.Individual, jmona.Individual)
+   * Partially matched crossover works in two stages. First, a random slice is
+   * swapped between the two tours, as in a two-point crossover. Second, any
+   * repeated cities in the non-swapped parts of the tours are replaced so the
+   * tours become valid again. This is done by using the mapping defined by
+   * <em>f({@code tour1.get(i)) = {@code tour2.get(i)}</em> for all <em>i</em>
+   * in the interval defined by the start and end indices of the swapped slice,
+   * and the inverse of this mapping. (If <em>f(x)</em> is still in the swapped
+   * section of the tour, then apply the mapping repeatedly to find the
+   * replacement city).
+   * 
+   * @param tour1
+   *          A tour.
+   * @param tour2
+   *          Another tour.
+   * @see jmona.CrossoverFunction#crossover(Object, Object)
    */
   @Override
   public void crossover(final List<Integer> tour1, final List<Integer> tour2) {
 
-    // choose a random start and end point for the slice
-    final int start = Util.RANDOM.nextInt(tour1.size());
-    final int end = start + Util.RANDOM.nextInt(tour1.size() - start);
+    // get the size of the tours
+    final int size = tour1.size();
 
-    LOG.debug("(start, end) = (" + start + ", " + end + ")");
+    // choose a random start point for the slice to swap
+    final int start = Util.RANDOM.nextInt(size);
 
-    // instantiate two child tours
-    final List<Integer> child1 = new Vector<Integer>();
-    final List<Integer> child2 = new Vector<Integer>();
+    // choose a random end point for the slice to swap (subtract one from the
+    // domain of random integers, then add one to the chosen random integer to
+    // ensure that the slice size is greater than 0)
+    final int end = start + Util.RANDOM.nextInt(size - start - 1) + 1;
 
-    // add the sublist in between the start and end points to the children
-    child1.addAll(tour1.subList(start, end));
-    child2.addAll(tour2.subList(start, end));
+    // crossover the section in between the start and end indices
+    Util.swap(tour1, tour2, start, end);
 
-    LOG.debug("Child 1: " + child1);
-    LOG.debug("Child 2: " + child2);
+    // get a view of the crossover over sections in each tour
+    final List<Integer> swappedSectionInTour1 = tour1.subList(start, end);
+    final List<Integer> swappedSectionInTour2 = tour2.subList(start, end);
+
+    int currentCity = 0;
+    int replacementCityIndex = 0;
+    int replacementCity = 0;
+
+    // iterate over each city in not in the crossed over section
+    for (int i = end; i >= end || i < start; i = (i + 1) % size) {
+
+      // get the current city being examined in tour 1
+      currentCity = tour1.get(i);
+
+      // if that city is repeated in the crossed over section
+      if (swappedSectionInTour1.contains(currentCity)) {
+
+        // get the index of the city to replace the repeated city (within the
+        // swapped section)
+        replacementCityIndex = swappedSectionInTour1.indexOf(currentCity);
+
+        // get the city that is intended to replace the repeated city
+        replacementCity = swappedSectionInTour2.get(replacementCityIndex);
+
+        // if the repeated city is also contained in the crossed over section
+        while (swappedSectionInTour1.contains(replacementCity)) {
+
+          // get the index of the city to replace the repeated city
+          replacementCityIndex = swappedSectionInTour1.indexOf(replacementCity);
+
+          // get the city that is intended to replace the repeated city
+          replacementCity = swappedSectionInTour2.get(replacementCityIndex);
+
+        }
+
+        // replace the current city with the replacement city
+        tour1.set(i, replacementCity);
+      }
+
+      // get the current city being examined in tour 2
+      currentCity = tour2.get(i);
+
+      // if that city is repeated in the crossed over section
+      if (swappedSectionInTour2.contains(currentCity)) {
+
+        // get the index of the city to replace the repeated city
+        replacementCityIndex = swappedSectionInTour2.indexOf(currentCity);
+
+        // get the city that is intended to replace the repeated city
+        replacementCity = swappedSectionInTour1.get(replacementCityIndex);
+
+        // if the repeated city is also contained in the crossed over section
+        while (swappedSectionInTour2.contains(replacementCity)) {
+
+          // get the index of the city to replace the repeated city
+          replacementCityIndex = swappedSectionInTour2.indexOf(replacementCity);
+
+          // get the city that is intended to replace the repeated city
+          replacementCity = swappedSectionInTour1.get(replacementCityIndex);
+        }
+
+        // replace the current city with the replacement city
+        tour2.set(i, replacementCity);
+      }
+    }
 
   }
-
 }
