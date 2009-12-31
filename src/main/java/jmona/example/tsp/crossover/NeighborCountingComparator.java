ebe55ee3d6c13a1153cd77f51743df4be144a9cb
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2009-12-30 23:51:38 -0500

    added a comparator which compares the number of neighbors of two cities given an adjacency list

---
+++ src/main/java/jmona/example/tsp/crossover/NeighborCountingComparator.java
@@ -0,0 +1,64 @@
+/**
+ * NeighborCountingComparator.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.example.tsp.crossover;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A comparator which counts the number of neighbors of a city in a traveling
+ * salesman problem tour.
+ * 
+ * @author jfinkels
+ */
+class NeighborCountingComparator implements Comparator<Integer> {
+
+  /** The adjacency list as a mapping from city to neighbors of that city. */
+  private final Map<Integer, Set<Integer>> neighborSets;
+
+  /**
+   * Instantiate this Comparator with the specified adjacency list.
+   * 
+   * @param initialNeighborSets
+   *          The mapping from city to neighbors of that city.
+   */
+  public NeighborCountingComparator(
+      final Map<Integer, Set<Integer>> initialNeighborSets) {
+    this.neighborSets = initialNeighborSets;
+  }
+
+  /**
+   * Compare the number of neighbors of the specified cities.
+   * 
+   * @param city1
+   *          A city.
+   * @param city2
+   *          Another city.
+   * @return The difference between the two specified cities.
+   * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+   */
+  @Override
+  public int compare(final Integer city1, final Integer city2) {
+    return this.neighborSets.get(city1).size()
+        - this.neighborSets.get(city2).size();
+  }
+
+}
---
+++ src/test/java/jmona/example/tsp/crossover/NeighborCountingComparatorTester.java
@@ -0,0 +1,88 @@
+/**
+ * NeighborCountingComparatorTester.java
+ * 
+ * Copyright 2009 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+package jmona.example.tsp.crossover;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import jmona.impl.Range;
+
+import org.junit.Test;
+
+/**
+ * Test class for the NeighborCountingComparator class.
+ * 
+ * @author jfinkels
+ */
+public class NeighborCountingComparatorTester {
+
+  /** The number of cities in the tour. */
+  public static final int SIZE = 10;
+
+  /**
+   * Test method for
+   * {@link jmona.example.tsp.crossover.NeighborCountingComparator#compare(java.lang.Integer, java.lang.Integer)}
+   * .
+   */
+  @Test
+  public void testCompare() {
+    final Map<Integer, Set<Integer>> neighborSets = new HashMap<Integer, Set<Integer>>();
+
+    // iterate over the integers from 0 to SIZE
+    Set<Integer> neighbors = null;
+    for (final int i : new Range(SIZE)) {
+      neighbors = new HashSet<Integer>();
+
+      // add the same number of neighbors as the number of the city
+      for (final int j : new Range(i)) {
+        neighbors.add(j);
+      }
+
+      neighborSets.put(i, neighbors);
+    }
+
+    // create a comparator based on the created adjacency list
+    final NeighborCountingComparator comparator = new NeighborCountingComparator(
+        neighborSets);
+
+    // iterate over each city
+    for (final int i : new Range(SIZE)) {
+
+      // every city with number less than the current city has fewer neighbors
+      for (final int j : new Range(i)) {
+        assertTrue(comparator.compare(j, i) < 0);
+      }
+
+      // every city always has the same number of neighbors as itself
+      assertEquals(0, comparator.compare(i, i));
+
+      // every city with number greater than the current city has more neighbors
+      for (final int j : new Range(i + 1, SIZE)) {
+        assertTrue(comparator.compare(j, i) > 0);
+      }
+    }
+  }
+
+}
