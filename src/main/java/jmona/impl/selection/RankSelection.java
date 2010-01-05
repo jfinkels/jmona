45b63e625e40de70109fa68278fa399e13d7ea9f
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2010-01-05 01:08:48 -0500

    implemented RankSelection

--- src/main/java/jmona/impl/selection/RankSelection.java
+++ src/main/java/jmona/impl/selection/RankSelection.java
@@ -19,27 +19,89 @@
  */
 package jmona.impl.selection;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Vector;
 
 import jmona.FitnessFunction;
 import jmona.IndependentSelectionFunction;
+import jmona.SelectionException;
+import jmona.functional.Range;
+import jmona.impl.fitness.PresetFitnessFunction;
+import jmona.impl.fitness.StandardizedFitnessComparator;
 
 /**
+ * SelectionFunction which assigns a linear rank to individuals (1, 2, 3,
+ * &hellip;), then uses those numbers as the adjusted fitness in a
+ * fitness-proportionate selection to select an individual.
+ * 
  * @author Jeffrey Finkelstein
+ * @param <T>
+ *          The type of individual to choose.
  * @since 0.3
  */
 public class RankSelection<T> implements IndependentSelectionFunction<T> {
 
-  /*
-   * (non-Javadoc)
+  /** The comparator with which to rank the individuals in the population. */
+  private final StandardizedFitnessComparator<T> standardizedFitnessComparator = new StandardizedFitnessComparator<T>();
+
+  /**
+   * Use a linear ranking as the probability distribution with which to choose
+   * an individual from the specified population.
    * 
+   * Linear ranking is performed by assigning a rank (1, 2, 3, &hellip;, {@code
+   * population.size()}) to each individual in the specified population. This
+   * number is then used as the fitness of the individual in a
+   * fitness-proportionate selection.
+   * 
+   * @param population
+   *          The population from which to choose an individual.
+   * @param fitnessFunction
+   *          The fitness function which is used to determine ranking of
+   *          individuals of the specified population.
+   * @return An individual chosen from the specified population.
+   * @throws SelectionException
+   *           If the fitness-proportionate selection phase of this method
+   *           throws a SelectionException.
    * @see jmona.SelectionFunction#select(java.util.List, jmona.FitnessFunction)
    */
   @Override
   public T select(final List<T> population,
-      final FitnessFunction<T> fitnessFunction) {
-    // TODO Auto-generated method stub
-    return null;
-  }
+      final FitnessFunction<T> fitnessFunction) throws SelectionException {
 
+    // make the fitness comparator aware of the fitness function
+    this.standardizedFitnessComparator.setFitnessFunction(fitnessFunction);
+
+    // create a new list containing references to all the individuals in the
+    // population (so as not to upset the order of the elements in the
+    // population list)
+    final List<T> individuals = new Vector<T>(population);
+
+    // sort the population based on fitness (better individuals first)
+    Collections.sort(individuals, this.standardizedFitnessComparator);
+
+    // reverse the order so that better individuals have a higher index in the
+    // list
+    Collections.reverse(individuals);
+
+    // define a map from individual to its rank (that is, its index in the list)
+    final Map<T, Double> rank = new HashMap<T, Double>();
+    for (final int i : new Range(individuals.size())) {
+      rank.put(individuals.get(i), (double) (i + 1));
+    }
+
+    // create a fitness function which uses that Map to determine adjusted
+    // fitnesses
+    final FitnessFunction<T> presetFitnessFunction = new PresetFitnessFunction<T>(
+        rank);
+
+    // use fitness proportionate selection where the adjusted fitness is the
+    // rank of the individual
+    final FitnessProportionateSelection<T> fitnessProportionateSelection = new FitnessProportionateSelection<T>();
+
+    return fitnessProportionateSelection.select(individuals,
+        presetFitnessFunction);
+  }
 }
--- src/test/java/jmona/impl/selection/RankSelectionTester.java
+++ src/test/java/jmona/impl/selection/RankSelectionTester.java
@@ -19,22 +19,78 @@
  */
 package jmona.impl.selection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Vector;
+
+import jmona.SelectionException;
+import jmona.functional.Range;
+import jmona.impl.example.ExampleFitnessFunction;
+import jmona.impl.example.ExampleIndividual;
+import jmona.test.Util;
 
 import org.junit.Test;
 
 /**
+ * Test class for the RankSelection class.
+ * 
  * @author Jeffrey Finkelstein
  * @since 0.3
  */
 public class RankSelectionTester {
 
+  /** The number of selections to make. */
+  public static final int NUM_SELECTIONS = 10000;
+
   /**
-   * Test method for {@link jmona.impl.selection.RankSelection#select(java.util.List, jmona.FitnessFunction)}.
+   * Test method for
+   * {@link jmona.impl.selection.RankSelection#select(java.util.List, jmona.FitnessFunction)}
+   * .
    */
   @Test
   public void testSelect() {
-    fail("Not yet implemented");
+    final RankSelection<ExampleIndividual> function = new RankSelection<ExampleIndividual>();
+    final ExampleFitnessFunction fitnessFunction = new ExampleFitnessFunction();
+    fitnessFunction.setExtremum(0.0);
+
+    final ExampleIndividual individual1 = new ExampleIndividual(1);
+    final ExampleIndividual individual2 = new ExampleIndividual(2);
+    final ExampleIndividual individual3 = new ExampleIndividual(3);
+
+    final List<ExampleIndividual> population = new Vector<ExampleIndividual>();
+    population.add(individual1);
+    population.add(individual2);
+    population.add(individual3);
+
+    int selectionsOfIndividual1 = 0;
+    int selectionsOfIndividual2 = 0;
+    int selectionsOfIndividual3 = 0;
+
+    try {
+      ExampleIndividual selection = null;
+      for (final int i : new Range(NUM_SELECTIONS)) {
+        selection = function.select(population, fitnessFunction);
+
+        if (selection.equals(individual1)) {
+          selectionsOfIndividual1 += 1;
+        } else if (selection.equals(individual2)) {
+          selectionsOfIndividual2 += 1;
+        } else {
+          selectionsOfIndividual3 += 1;
+        }
+      }
+    } catch (final SelectionException exception) {
+      Util.fail(exception);
+    }
+
+    final double expected1 = NUM_SELECTIONS * (3.0 / 6.0);
+    final double expected2 = NUM_SELECTIONS * (2.0 / 6.0);
+    final double expected3 = NUM_SELECTIONS * (1.0 / 6.0);
+
+    assertEquals(expected1, selectionsOfIndividual1, expected1 * 0.1);
+    assertEquals(expected2, selectionsOfIndividual2, expected2 * 0.1);
+    assertEquals(expected3, selectionsOfIndividual3, expected3 * 0.1);
   }
 
 }
