0c4299b3bed21e77bd729210af998603b278182c
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2010-01-04 19:21:31 -0500

    implemented MaximizingFitnessFunction test

--- src/test/java/jmona/impl/fitness/MaximizingFitnessFunctionTester.java
+++ src/test/java/jmona/impl/fitness/MaximizingFitnessFunctionTester.java
@@ -19,30 +19,50 @@
  */
 package jmona.impl.fitness;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import jmona.FitnessException;
+import jmona.impl.example.ExampleIndividual;
+import jmona.test.Util;
 
 import org.junit.Test;
 
 /**
+ * Test class for the MaximizingFitnessFunction class.
+ * 
  * @author Jeffrey Finkelstein
  * @since 0.3
  */
 public class MaximizingFitnessFunctionTester {
 
+  /** Zero. */
+  public static final double ZERO_DELTA = 0.0;
+
   /**
-   * Test method for {@link jmona.impl.fitness.MaximizingFitnessFunction#typeOfExtremum()}.
+   * Test method for
+   * {@link jmona.impl.fitness.MaximizingFitnessFunction#typeOfExtremum()}.
    */
   @Test
   public void testTypeOfExtremum() {
-    fail("Not yet implemented");
-  }
+    final double maximum = 10;
+    final MaximizingFitnessFunction<ExampleIndividual> function = new MaximizingFitnessFunction<ExampleIndividual>(
+        maximum) {
+      @Override
+      public double rawFitness(final ExampleIndividual individual) {
+        return individual.fitness();
+      }
+    };
+    assertEquals(KnownExtremumFitnessFunction.MAXIMUM, function
+        .typeOfExtremum());
 
-  /**
-   * Test method for {@link jmona.impl.fitness.MaximizingFitnessFunction#MaximizingFitnessFunction(double)}.
-   */
-  @Test
-  public void testMaximizingFitnessFunction() {
-    fail("Not yet implemented");
+    try {
+      assertEquals(maximum, function.standardizedFitness(new ExampleIndividual(
+          0)), ZERO_DELTA);
+      assertEquals(0, function.standardizedFitness(new ExampleIndividual(
+          maximum)), ZERO_DELTA);
+    } catch (final FitnessException exception) {
+      Util.fail(exception);
+    }
+
   }
 
 }
