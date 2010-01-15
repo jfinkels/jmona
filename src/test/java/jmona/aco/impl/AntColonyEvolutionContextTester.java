daa9fa6ff4c83b046b2467c8637ebfcf8249bbfb
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2010-01-15 00:10:14 -0500

    added some tests for ant colony evolution contexts

--- src/test/java/jmona/aco/impl/AntColonyEvolutionContextTester.java
+++ src/test/java/jmona/aco/impl/AntColonyEvolutionContextTester.java
@@ -19,54 +19,135 @@
  */
 package jmona.aco.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+import java.util.Vector;
+
+import jmona.EvolutionException;
+import jmona.ProcessingException;
+import jmona.aco.PheromoneUpdateStrategy;
+import jmona.functional.Range;
+import jmona.test.Util;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Test class for the AntColonyEvolutionContext class.
+ * 
  * @author Jeffrey Finkelstein
  * @since 0.5
  */
 public class AntColonyEvolutionContextTester {
 
+  /** The initial pheromone to place on each edge in the graph under test. */
+  public static final double INITIAL_PHEROMONE = 10;
+  /** The number of vertices to in the graph under test. */
+  public static final int NUMBER_OF_VERTICES = 10;
+  /** The pheromone update strategy to use in the evolution. */
+  public static final PheromoneUpdateStrategy STRATEGY = new AntCycleStrategy();
+  /** Zero. */
+  public static final double ZERO_DELTA = 0.0;
+  /** The adjacency matrix for the graph under test. */
+  private double[][] adjacencyMatrix = null;
+  /** The AntColonyEvolutionContext under test. */
+  private AntColonyEvolutionContext<WorkerAnt> context = null;
+  /** The graph under test. */
+  private DefaultPheromoneDirectedGraph graph = null;
+  /** The population of Ants in the evolution. */
+  private List<WorkerAnt> population = null;
+
+  @Before
+  public void setUp() {
+    this.adjacencyMatrix = new double[NUMBER_OF_VERTICES][NUMBER_OF_VERTICES];
+
+    for (final int i : new Range(NUMBER_OF_VERTICES)) {
+      for (final int j : new Range(NUMBER_OF_VERTICES)) {
+        this.adjacencyMatrix[i][j] = i + j;
+      }
+    }
+
+    this.graph = new DefaultPheromoneDirectedGraph(this.adjacencyMatrix,
+        INITIAL_PHEROMONE);
+
+    this.population = new Vector<WorkerAnt>();
+
+    for (final int i : new Range(NUMBER_OF_VERTICES)) {
+      this.population.add(new WorkerAnt(i));
+    }
+
+    this.context = new AntColonyEvolutionContext<WorkerAnt>(this.population,
+        this.graph, STRATEGY);
+  }
+
   /**
-   * Test method for {@link jmona.aco.impl.AntColonyEvolutionContext#executeGenerationStep()}.
+   * Test method for
+   * {@link jmona.aco.impl.AntColonyEvolutionContext#executeGenerationStep()}.
    */
   @Test
   public void testExecuteGenerationStep() {
-    fail("Not yet implemented");
+    try {
+      this.context.executeGenerationStep();
+    } catch (final EvolutionException exception) {
+      Util.fail(exception);
+    }
+
+    for (final WorkerAnt ant : this.context.currentPopulation()) {
+      for (final int j : new Range(NUMBER_OF_VERTICES)) {
+        assertEquals(ant.memory().indexOf(j), ant.memory().lastIndexOf(j));
+      }
+    }
   }
 
   /**
-   * Test method for {@link jmona.aco.impl.AntColonyEvolutionContext#AntColonyEvolutionContext(java.util.List, jmona.aco.PheromoneDirectedGraph, jmona.aco.PheromoneUpdateStrategy)}.
-   */
-  @Test
-  public void testAntColonyEvolutionContext() {
-    fail("Not yet implemented");
-  }
-
-  /**
-   * Test method for {@link jmona.aco.impl.AntColonyEvolutionContext#setPheromonePersistence(double)}.
-   */
-  @Test
-  public void testSetPheromonePersistence() {
-    fail("Not yet implemented");
-  }
-
-  /**
-   * Test method for {@link jmona.aco.impl.AntColonyEvolutionContext#setPheromoneImportance(double)}.
+   * Test method for
+   * {@link jmona.aco.impl.AntColonyEvolutionContext#setPheromoneImportance(double)}
+   * .
    */
   @Test
   public void testSetPheromoneImportance() {
-    fail("Not yet implemented");
+    final double newPheromoneImportance = 0.999;
+    this.context.setPheromoneImportance(newPheromoneImportance);
+    // TODO assertions
   }
 
   /**
-   * Test method for {@link jmona.aco.impl.AntColonyEvolutionContext#setVisibilityImportance(double)}.
+   * Test method for
+   * {@link jmona.aco.impl.AntColonyEvolutionContext#setPheromonePersistence(double)}
+   * .
+   */
+  @Test
+  public void testSetPheromonePersistence() {
+    this.context = new AntColonyEvolutionContext<WorkerAnt>(
+        new Vector<WorkerAnt>(), this.graph, new AntCycleStrategy());
+
+    final double newPheromonePersistence = 0.9;
+    this.context.setPheromonePersistence(newPheromonePersistence);
+    try {
+      this.context.stepGeneration();
+    } catch (final EvolutionException exception) {
+      Util.fail(exception);
+    }
+
+    for (final int i : new Range(NUMBER_OF_VERTICES)) {
+      for (final int j : new Range(NUMBER_OF_VERTICES)) {
+        assertEquals(INITIAL_PHEROMONE * newPheromonePersistence, this.graph
+            .pheromoneBetween(i, j), ZERO_DELTA);
+      }
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link jmona.aco.impl.AntColonyEvolutionContext#setVisibilityImportance(double)}
+   * .
    */
   @Test
   public void testSetVisibilityImportance() {
-    fail("Not yet implemented");
+    final double newVisibilityImportance = 0.999;
+    this.context.setVisibilityImportance(newVisibilityImportance);
+    // TODO assertions
   }
 
 }
--- src/test/java/jmona/aco/impl/TourEvolutionContextTester.java
+++ src/test/java/jmona/aco/impl/TourEvolutionContextTester.java
@@ -19,54 +19,156 @@
  */
 package jmona.aco.impl;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+
+import jmona.EvolutionException;
+import jmona.SelectionException;
+import jmona.aco.PheromoneUpdateStrategy;
+import jmona.functional.Functional;
+import jmona.functional.Range;
+import jmona.test.Util;
+
+import org.junit.Before;
 import org.junit.Test;
 
 /**
+ * Test class for the TourEvolutionContext class.
+ * 
  * @author Jeffrey Finkelstein
  * @since 0.5
  */
 public class TourEvolutionContextTester {
 
-  /**
-   * Test method for {@link jmona.aco.impl.TourEvolutionContext#executeGenerationStep()}.
-   */
-  @Test
-  public void testExecuteGenerationStep() {
-    fail("Not yet implemented");
+  /** The initial pheromone to place on each edge in the graph under test. */
+  public static final double INITIAL_PHEROMONE = 10;
+  /** The number of selections to make for the next vertex to which to move. */
+  public static final int NUM_SELECTIONS = 100000;
+  /** The number of vertices to in the graph under test. */
+  public static final int NUMBER_OF_VERTICES = 3;
+  /** The pheromone update strategy to use in the evolution. */
+  public static final PheromoneUpdateStrategy STRATEGY = new AntCycleStrategy();
+  /** Zero. */
+  public static final double ZERO_DELTA = 0.0;
+  /** The adjacency matrix for the graph under test. */
+  private double[][] adjacencyMatrix = null;
+  /** The TourEvolutionContext under test. */
+  private TourEvolutionContext<WorkerAnt> context = null;
+  /** The graph under test. */
+  private DefaultPheromoneDirectedGraph graph = null;
+
+  /** The population of Ants in the evolution. */
+  private List<WorkerAnt> population = null;
+
+  /** Establish a fixture for tests in this class. */
+  @Before
+  public void setUp() {
+    this.adjacencyMatrix = new double[NUMBER_OF_VERTICES][NUMBER_OF_VERTICES];
+
+    for (final int i : new Range(NUMBER_OF_VERTICES)) {
+      for (final int j : new Range(NUMBER_OF_VERTICES)) {
+        this.adjacencyMatrix[i][j] = i + j;
+      }
+    }
+
+    this.graph = new DefaultPheromoneDirectedGraph(this.adjacencyMatrix,
+        INITIAL_PHEROMONE);
+
+    this.population = new Vector<WorkerAnt>();
+
+    for (final int i : new Range(NUMBER_OF_VERTICES)) {
+      this.population.add(new WorkerAnt(i));
+    }
+
+    this.context = new TourEvolutionContext<WorkerAnt>(this.population,
+        this.graph, STRATEGY);
   }
 
   /**
-   * Test method for {@link jmona.aco.impl.TourEvolutionContext#TourEvolutionContext(java.util.List, jmona.aco.PheromoneDirectedGraph, jmona.aco.PheromoneUpdateStrategy)}.
-   */
-  @Test
-  public void testTourEvolutionContext() {
-    fail("Not yet implemented");
-  }
-
-  /**
-   * Test method for {@link jmona.aco.impl.TourEvolutionContext#chooseNextVertex(jmona.aco.Ant)}.
+   * Test method for
+   * {@link jmona.aco.impl.TourEvolutionContext#chooseNextVertex(jmona.aco.Ant)}
+   * .
    */
   @Test
   public void testChooseNextVertex() {
-    fail("Not yet implemented");
+    final WorkerAnt ant = this.population.get(0);
+
+    final Map<Integer, Integer> selections = new HashMap<Integer, Integer>();
+    for (final int i : new Range(NUMBER_OF_VERTICES)) {
+      selections.put(i, 0);
+    }
+
+    try {
+      int selection = 0;
+      for (final int i : new Range(NUM_SELECTIONS)) {
+        selection = this.context.chooseNextVertex(ant);
+        selections.put(selection, selections.get(selection) + 1);
+      }
+    } catch (final SelectionException exception) {
+      Util.fail(exception);
+    }
+
+    final int totalSelections = Functional.sum(selections.values());
+    assertEquals(totalSelections, NUM_SELECTIONS);
+
+    assertEquals(0, selections.get(0).intValue());
+    for (final int i : new Range(2, NUMBER_OF_VERTICES)) {
+      assertTrue(selections.get(i - 1) > selections.get(i));
+    }
+    
+    final double delta = selections.get(2) * 0.1;
+    if (NUMBER_OF_VERTICES == 3) {
+      assertEquals(selections.get(1), selections.get(2) * 2, delta);
+    }
   }
 
   /**
-   * Test method for {@link jmona.aco.impl.TourEvolutionContext#setPheromoneImportance(double)}.
+   * Test method for
+   * {@link jmona.aco.impl.TourEvolutionContext#executeGenerationStep()}.
+   */
+  @Test
+  public void testExecuteGenerationStep() {
+    for (final int i : new Range(NUMBER_OF_VERTICES - 1)) {
+      try {
+        this.context.executeGenerationStep();
+      } catch (final EvolutionException exception) {
+        Util.fail(exception);
+      }
+
+      for (final WorkerAnt ant : this.context.currentPopulation()) {
+        for (final int j : new Range(NUMBER_OF_VERTICES)) {
+          assertEquals(ant.memory().indexOf(j), ant.memory().lastIndexOf(j));
+        }
+      }
+    }
+  }
+
+  /**
+   * Test method for
+   * {@link jmona.aco.impl.TourEvolutionContext#setPheromoneImportance(double)}.
    */
   @Test
   public void testSetPheromoneImportance() {
-    fail("Not yet implemented");
+    final double newPheromoneImportance = 0.999;
+    this.context.setPheromoneImportance(newPheromoneImportance);
+    // TODO assertions
   }
 
   /**
-   * Test method for {@link jmona.aco.impl.TourEvolutionContext#setVisibilityImportance(double)}.
+   * Test method for
+   * {@link jmona.aco.impl.TourEvolutionContext#setVisibilityImportance(double)}
+   * .
    */
   @Test
   public void testSetVisibilityImportance() {
-    fail("Not yet implemented");
+    final double newVisibilityImportance = 0.999;
+    this.context.setVisibilityImportance(newVisibilityImportance);
+    // TODO assertions
   }
 
 }
