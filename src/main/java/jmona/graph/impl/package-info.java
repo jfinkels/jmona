245924cbb3ce9b91658fa541c981264f03bf2734
Jeffrey Finkelstein <jeffrey.finkelstein@gmail.com>
2010-08-21 13:01:40 -0400

    added package level documentation

---
+++ src/main/java/jmona/graph/impl/package-info.java
@@ -0,0 +1,30 @@
+/**
+ * package-info.java
+ * 
+ * Copyright 2010 Jeffrey Finkelstein
+ * 
+ * This file is part of jmona.
+ * 
+ * jmona is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * jmona is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * jmona. If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * <p>
+ * Provides a simple adjacency matrix implementation of a graph with
+ * {@code integer} vertices and {@code double} edge weights.
+ * </p>
+ * 
+ * @author Jeffrey Finkelstein
+ * @since 0.5
+ */
+package jmona.graph.impl;
+
